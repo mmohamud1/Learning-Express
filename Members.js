c2c685f402b8fa7098d43750ce3b4736f605045d
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-09-29 19:33:10 +0200

    Commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Johnson',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Steve Jobs',
+        email: 'steve@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
+
+
+
+// Init middleware
+//app.use(logger)
+
+// Gets all members
+app.get('/api/members', (req, res) => {
+    res.json(members)
+});
+
+// Get single member 
+app.get('/ap/members/:id', (req, res) => {
+    res.json(members.filter(member => member.id === req.params.id))
+})
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
